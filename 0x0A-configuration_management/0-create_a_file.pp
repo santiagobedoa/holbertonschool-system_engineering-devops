148abd823a56689d09d76381bb54a74443bfca75
Santiago Bedoya <santiagobedoa@gmail.com>
2022-08-19 17:36:42 -0500

    Fri Aug 19 17:36:42 -05 2022

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with specific requirements
+
+file { 'holberton':
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
