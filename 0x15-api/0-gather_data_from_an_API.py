ba432ed99888cfd76dad672e357a30ccc115ed0d
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-09 13:43:55 -0500

    Fri Sep  9 13:43:55 -05 2022

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,23 +9,36 @@ from sys import argv
 
 
 def get_info():
-    url_employee = "https://jsonplaceholder.typicode.com/users/"
-    employee_info = (requests.get("{}{}".format(url_employee, argv[1]))).json()
-    url_tasks = "https://jsonplaceholder.typicode.com/todos?userId="
-    tasks_info = (requests.get("{}{}".format(url_tasks, argv[1]))).json()
-    total_tasks = 0
-    completed_tasks = list()
-    for task in tasks_info:
-        total_tasks += 1
-        if task["completed"] is True:
-            completed_tasks.append(task)
-    print("Employee {} is done with tasks ({}/{}):".format(
-        employee_info["name"],
-        len(completed_tasks),
-        total_tasks,
-    ))
-    for task in completed_tasks:
-        print("\t{}".format(task["title"]))
+    # ORIGINAL ANSWER
+    # url_employee = "https://jsonplaceholder.typicode.com/users/"
+    # employee_info = (requests.get("{}{}".format(url_employee, argv[1]))).json()
+    # url_tasks = "https://jsonplaceholder.typicode.com/todos?userId="
+    # tasks_info = (requests.get("{}{}".format(url_tasks, argv[1]))).json()
+    # total_tasks = 0
+    # completed_tasks = list()
+    # for task in tasks_info:
+    #     total_tasks += 1
+    #     if task["completed"] is True:
+    #         completed_tasks.append(task)
+    # print("Employee {} is done with tasks ({}/{}):".format(
+    #     employee_info["name"],
+    #     len(completed_tasks),
+    #     total_tasks,
+    # ))
+    # for task in completed_tasks:
+    #     print("\t {}".format(task["title"]))
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    completed_tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            completed_tasks.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    print("\n".join("\t {}".format(task) for task in completed_tasks))
 
 
 if __name__ == "__main__":
